22dbc57e5a93afb75ca21009e3b5414fc36f1395
Rakotonarivo Mickael <mickaelrakotonarivo@gmail.com>
2024-10-12 21:24:49 +0300

    Sent location to server

--- image.py
+++ image.py
@@ -4,31 +4,63 @@ import cv2
 import os
 import time
 import numpy as np
+import requests
+import json
+import geocoder
 
-# Charger les deux modèles
 model = YOLO('models/trainedYolo.pt')
 modelYOLO = YOLO('models/yolov10n.pt')
 
-# Obtenir les vocabulaires des deux modèles
-vocab1 = model.names  # Returns a dictionary
-vocab2 = modelYOLO.names  # Returns a dictionary
+vocab1 = model.names
+vocab2 = modelYOLO.names
 
-# Convert dictionaries to lists of class names
 vocab1_list = list(vocab1.values())
 vocab2_list = list(vocab2.values())
 
-# Merge the vocabularies
 merged_vocab = list(set(vocab1_list + vocab2_list))
 
-# Créer les mappings pour les classes de chaque modèle vers le vocabulaire fusionné
 mapping1 = {i: merged_vocab.index(name) for i, name in vocab1.items()}
 mapping2 = {i: merged_vocab.index(name) for i, name in vocab2.items()}
 
 last_capture_time = 0
-capture_interval = 3
+capture_interval = 2
 last_class_detected_name = None
 last_image = None
-pixel_diff_threshold = 25000
+pixel_diff_threshold = 5000000
+
+def get_phone_location():
+    g = geocoder.ip('102.19.119.251')
+    if g.ok:
+        return g.latlng
+    else:
+        print("Impossible de récupérer la localisation")
+        return None, None
+    
+def send_image_with_metadata(image_path, class_name, accident_timestamp, server_url):
+    latitude, longitude = get_phone_location()  
+    print(latitude, longitude)
+    
+    with open(image_path, 'rb') as image_file:
+        metadata = {
+            'userId': 1, 
+            'typeId': 1,
+            'latitude': latitude,
+            'longitude': longitude,
+            'description': f'Accident détecté par Caméra : {class_name}',
+            'timestamp': accident_timestamp
+        }
+
+        files = {'assets': image_file}
+        data = {'signal': json.dumps(metadata)}
+
+        try:
+            response = requests.post(server_url, files=files, data=data)
+            if response.status_code == 200:
+                print(f"Image et métadonnées envoyées avec succès à {server_url}")
+            else:
+                print(f"Échec de l'envoi : {response.status_code}")
+        except Exception as e:
+            print(f"Erreur lors de l'envoi : {e}")
 
 def save_image(image, class_name):
     if not os.path.exists('captures'):
@@ -38,15 +70,20 @@ def save_image(image, class_name):
     filename = f'captures/{class_name}_{timestamp}.png'
     
     cv2.imwrite(filename, image)
-    print(f"Saved: {filename}")
+    print(f"Image sauvegardée : {filename}")
+    
+    server_url = 'http://localhost:8080/api/signal'
+    
+    if class_name == 'accident':
+        accident_timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        send_image_with_metadata(filename, class_name, accident_timestamp, server_url)
 
 def process_image(image):
     global last_capture_time, last_class_detected_name, last_image
 
-    # Appliquer le premier modèle (accident-viewer.pt)
     results = model(image)
     
-    accident_detected = False  # Flag to track if an accident is detected
+    accident_detected = False
     
     for info in results:
         parameters = info.boxes
@@ -54,25 +91,20 @@ def process_image(image):
             x1, y1, x2, y2 = box.xyxy[0].numpy().astype('int')
             confidence = box.conf[0].numpy().astype('int') * 100
             class_detected_number = int(box.cls[0])
-            class_detected_name = vocab1[class_detected_number]  # Nom de classe original
+            class_detected_name = vocab1[class_detected_number]
             
-            # Récupérer le nom de classe fusionné
             merged_class_name = merged_vocab[mapping1[class_detected_number]]
 
-            # Dessiner le rectangle et le label
             cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 3)
             cvzone.putTextRect(image, f'{merged_class_name}', [x1 + 8, y1 - 12], thickness=2, scale=1.5)
             
-            # Logique de détection d'accident
             current_time = time.time()
             if merged_class_name in ['accident']:
                 print(f"{merged_class_name} detected!")
-                accident_detected = True  # Set the flag when an accident is detected
+                accident_detected = True 
 
-                # Capture seulement si le délai est respecté ou nouvelle détection
                 if (current_time - last_capture_time > capture_interval) or (merged_class_name != last_class_detected_name):
 
-                    # Vérifier la différence de pixels entre les images
                     if last_image is not None:
                         diff = cv2.absdiff(last_image, image)
                         non_zero_count = np.count_nonzero(diff)
@@ -82,15 +114,14 @@ def process_image(image):
                         if non_zero_count > pixel_diff_threshold:
                             last_capture_time = current_time
                             last_class_detected_name = merged_class_name
-                            last_image = image.copy()  # Update last_image for next comparison
-                            save_image(image, merged_class_name)  # Save the image directly here
+                            last_image = image.copy()
+                            save_image(image, merged_class_name)
                     else:
                         last_capture_time = current_time
                         last_class_detected_name = merged_class_name
-                        last_image = image.copy()  # Update last_image for next comparison
-                        save_image(image, merged_class_name)  # Save the image directly here
+                        last_image = image.copy()
+                        save_image(image, merged_class_name)
 
-    # Appliquer le second modèle (yolov10n.pt) si nécessaire
     results_yolo = modelYOLO(image)
     
     for info in results_yolo:
@@ -101,23 +132,9 @@ def process_image(image):
             class_detected_number = int(box.cls[0])
             class_detected_name = vocab2[class_detected_number]
             
-            # Récupérer le nom de classe fusionné
             merged_class_name = merged_vocab[mapping2[class_detected_number]]
 
-            # Dessiner le rectangle et le label
             cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 3)
             cvzone.putTextRect(image, f'{merged_class_name}', [x1 + 8, y1 - 12], thickness=2, scale=1.5)
 
     return image, accident_detected
-
-# Charger une image de test
-# image_path = 'Test/images/crash.jpg'
-# image = cv2.imread(image_path)
-
-# if image is None:
-#     print(f"Error: Could not load image from {image_path}")
-# else:
-#     processed_image, accident_detected = process_image(image)
-
-# # Clean up resources
-# cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,38 +1,32 @@
 import cv2
 import os
 import time
-from image import process_image  # Import the process_image function
+from image import process_image
 
-video_path = 'Test/videos/crash.mp4'  # Ensure the correct file extension
-cap = cv2.VideoCapture(video_path)  # Change 0 to the URL of the camera if needed
+cap = cv2.VideoCapture("http://192.168.43.1:8080/video")
 
-# Get the frames per second (fps) of the video
 fps = cap.get(cv2.CAP_PROP_FPS)
-interval_frames = int(fps * 2)  # Calculate the number of frames to skip (5 seconds)
+interval_frames = int(fps * 2)
 
-frame_count = 0  # Initialize frame count
-processed_image = None  # Initialize processed_image with None
+frame_count = 0
+processed_image = None
 
 while True:
     ret, image = cap.read()
     if not ret:
         break
 
-    frame_count += 1  # Increment the frame counter
+    frame_count += 1
 
-    # Process the image every 5 seconds
     if frame_count % interval_frames == 0:
         processed_image, accident_detected = process_image(image)
 
-        # Log message to indicate if an accident was detected
         if accident_detected:
             print("Accident detected! Capturing the image.")
     else:
-        # If not processing, use the original image for display
         processed_image = image
 
-    # Optionally, display the processed image (if needed)
-    cv2.imshow('frame', processed_image)  # Display the processed image
+    cv2.imshow('frame', processed_image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
